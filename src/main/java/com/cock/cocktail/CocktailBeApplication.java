3d68cd2fe04aa5f32138a2ceb83e4d2bdb9b1b79
Minuring <mw9505@gmail.com>
2026-03-18 14:01:28 +0900

    Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/cock/cocktail/CocktailBeApplication.java
@@ -0,0 +1,13 @@
+package com.cock.cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cock/cocktail/CocktailBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cock.cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
